acda02f9a8e91edbc0cf81b34bc203a24894e6b2
daywave <al295042@edu.uaa.mx>
2024-06-10 09:42:08 -0600

    rutas

--- routes/viajeRutas.js
+++ routes/viajeRutas.js
@@ -1,6 +1,6 @@
 // routes/sesionesRutas.js
 const express = require('express');
 const router = express.Router();
-const viajesController = require('../controllers/viajeController');
+const viajesController = require('../controllers/viajesController');
 
 router.get('/mostrarViajes', viajesController);
